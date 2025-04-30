31037c301fa273261de11e04f7d05aa6446faf15
jakkaphatCM <jakkaphat.m@pdkm.tech>
2025-04-30 10:21:03 +0700

    refactor : response :: refactor success res, add meta success res

--- pkg/custom/response.go
+++ pkg/custom/response.go
@@ -1,11 +1,45 @@
 package custom
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-type ResponseSuccess struct {
-	Data any `json:"data"`
-}
+	"github.com/gin-gonic/gin"
+)
+
+type (
+	// Data response
+	ResponseSuccess struct {
+		Data any `json:"data"`
+	}
+
+	// Meta response
+	MetaSuccessResponse struct {
+		Success SuccessDetail `json:"success"`
+	}
+
+	SuccessDetail struct {
+		Status  int               `json:"status"`
+		Name    string            `json:"name"`
+		Message string            `json:"message"`
+		Details map[string]string `json:"details,omitempty"`
+	}
+)
 
 func Success(pctx *gin.Context, statusCode int, data any) {
 	pctx.JSON(statusCode, &ResponseSuccess{Data: data})
+}
+
+func MetaSuccess(pctx *gin.Context, statusCode int, message string, details map[string]string) {
+	if details == nil {
+		details = make(map[string]string)
+	}
+
+	pctx.JSON(statusCode, &MetaSuccessResponse{
+		Success: SuccessDetail{
+			Status:  statusCode,
+			Name:    http.StatusText(statusCode),
+			Message: message,
+			Details: details,
+		},
+	})
 }
